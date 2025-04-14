2f20cd88456a0b171eb6de47130a0945385be1bf
ErmiyasTesfaye <ermiyastesfaye16@gmail.com>
2025-04-14 22:58:49 +0300

    mobile: Implement reusable widgets- sidebar and bottom navbar

---
+++ mobile_frontend/lib/core/widgets/bottom_nav_bar.dart
@@ -0,0 +1,77 @@
+// import 'package:flutter/material.dart';
+
+import 'package:flutter/material.dart';
+
+class BottomNavBar extends StatefulWidget {
+  const BottomNavBar({super.key});
+
+  @override
+  State<BottomNavBar> createState() => _BottomNavBarState();
+}
+
+class _BottomNavBarState extends State<BottomNavBar> {
+  int _currentIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return NavigationBar(
+      selectedIndex: _currentIndex,
+      onDestinationSelected: (int index) {
+        setState(() {
+          _currentIndex = index;
+        });
+      },
+      destinations: const [
+        NavigationDestination(
+          icon: Icon(Icons.storefront_rounded),
+          label: 'Market',
+        ),
+        NavigationDestination(
+          icon: Icon(Icons.message),
+          label: 'Message',
+        ),
+        NavigationDestination(
+          icon: Icon(Icons.shopping_cart),
+          label: 'Cart',
+        ),
+        NavigationDestination(
+          icon: Icon(Icons.person),
+          label: 'Me',
+        ),
+      ],
+    );
+  }
+}
+
+// class BottomNavBar extends StatelessWidget {
+//   const BottomNavBar({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return NavigationBar(
+//       selectedIndex: _currentIndex,
+//       onDestinationSelected: (int index) {
+//         setState(() {
+//           _currentIndex = index;
+//         });
+//       },
+//       destinations: const [
+//         NavigationDestination(
+//           icon: Icon(Icons.storefront_rounded),
+//           label: 'Market',
+//         ),
+//         NavigationDestination(
+//           icon: Icon(Icons.message),
+//           label: 'Message',
+//         ),
+//         NavigationDestination(
+//           icon: Icon(Icons.shopping_cart),
+//           label: 'Cart',
+//         ),
+//         NavigationDestination(
+//           icon: Icon(Icons.person),
+//           label: 'Me',
+//         ),
+//       ],
+//     );
+//   }
+// }
