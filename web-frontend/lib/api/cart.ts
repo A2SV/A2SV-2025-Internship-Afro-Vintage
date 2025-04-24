8d6ed31ece830577f9d9a6a4d92fc9ea11a1b406
SamrawitDawit <samrikdawit@gmail.com>
2025-04-24 14:49:07 +0300

    modified the url of the backend for cart api

--- web-frontend/lib/api/cart.ts
+++ web-frontend/lib/api/cart.ts
@@ -1,6 +1,6 @@
 import { Item } from '@/types/marketplace';
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8080';
+const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://2kps99nm-8080.uks1.devtunnels.ms';
 
 export interface CartResponse {
   success: boolean;
